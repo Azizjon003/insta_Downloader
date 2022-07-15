db2bca9a9646f5d1c1757acf61dd87bca49d8476
azizjon <aliqulovazizjon68@gmail.com>
2022-07-15 11:14:46 +0500

    xatolarni ushlad

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const axios = require("axios");
 const token = process.env.Token;
 const bot = new Telegraf(token);
 let son = 0;
-bot.command("start", async (msg, next) => {
+bot.command("start", async (msg) => {
   const id = msg.update.message.from.id;
   const name = msg.update.message.from.first_name;
 
@@ -31,83 +31,35 @@ bot.command("tags", async (msg) => {
   son = 3;
 });
 bot.on("text", async (msg) => {
-  const id = msg.update.message.from.id;
-  const name = msg.update.message.from.first_name;
-  const text = msg.update.message.text;
+  try {
+    const id = msg.update.message.from.id;
+    const name = msg.update.message.from.first_name;
+    const text = msg.update.message.text;
 
-  if (text == "Post yuklash") {
-    msg.telegram.sendMessage(id, `Post yuklash uchun linkni bizga yuboring`, {
-      reply_markup: {
-        remove_keyboard: true,
-      },
-    });
-    son = 1;
-  } else {
-    if (text == "Profil Rasmini yuklash") {
-      msg.telegram.sendMessage(
-        id,
-        `Profil rasmini yuklash uchun accaunt nomini bizga yuboring:Masalan cristiano`,
-        {
-          reply_markup: {
-            remove_keyboard: true,
-          },
-        }
-      );
-      son = 2;
+    if (text == "Post yuklash") {
+      msg.telegram.sendMessage(id, `Post yuklash uchun linkni bizga yuboring`, {
+        reply_markup: {
+          remove_keyboard: true,
+        },
+      });
+      son = 1;
     } else {
-      if (son == 1) {
-        const res = await Instagram.getAny(text);
-        console.log(res.data);
-        if (res.hasError) {
-          msg.telegram.sendMessage(id, `Dasturda xatolik bor`, {
+      if (text == "Profil Rasmini yuklash") {
+        msg.telegram.sendMessage(
+          id,
+          `Profil rasmini yuklash uchun accaunt nomini bizga yuboring:Masalan cristiano`,
+          {
             reply_markup: {
               remove_keyboard: true,
             },
-          });
-          return;
-        }
-        let data = await axios.get(res.data.body.link, {
-          responseType: "stream",
-        });
-
-        if (res.data.body.type == "GraphVideo") {
-          await data.data.pipe(fs.createWriteStream(`${__dirname}/${id}.mp4`));
-          msg.telegram.sendMessage(
-            id,
-            `Post yuklandi,U Faylni sizga yuboraylikmi`,
-            {
-              reply_markup: {
-                inline_keyboard: [
-                  [{ text: "Ha", callback_data: "haV" }],
-                  [{ text: "Yoq", callback_data: "yoq" }],
-                ],
-              },
-            }
-          );
-        }
-
-        if (res.data.body.type == "GraphImage") {
-          await data.data.pipe(fs.createWriteStream(`${__dirname}/${id}.jpg`));
-
-          msg.telegram.sendMessage(
-            id,
-            `Post yuklandi,U Faylni sizga yuboraylikmi`,
-            {
-              reply_markup: {
-                inline_keyboard: [
-                  [{ text: "Ha", callback_data: "haI" }],
-                  [{ text: "Yoq", callback_data: "yoq" }],
-                ],
-              },
-            }
-          );
-        }
+          }
+        );
+        son = 2;
       } else {
-        if (son == 2) {
-          console.log(text);
-          const rasm = await Instagram.getStories(text);
-
-          if (rasm.hasError) {
+        if (son == 1) {
+          const res = await Instagram.getAny(text);
+          console.log(res.data);
+          if (res.hasError) {
             msg.telegram.sendMessage(id, `Dasturda xatolik bor`, {
               reply_markup: {
                 remove_keyboard: true,
@@ -115,103 +67,140 @@ bot.on("text", async (msg) => {
             });
             return;
           }
-          console.log(rasm.data);
-          data = await axios.get(rasm.data.body.profile.profile_pic_url, {
+          let data = await axios.get(res.data.body.link, {
             responseType: "stream",
           });
 
-          await data.data.pipe(fs.createWriteStream(`${__dirname}/${id}.jpg`));
+          if (res.data.body.type == "GraphVideo") {
+            await data.data.pipe(
+              fs.createWriteStream(`${__dirname}/${id}.mp4`)
+            );
+            msg.telegram.sendMessage(
+              id,
+              `Post yuklandi,U Faylni sizga yuboraylikmi`,
+              {
+                reply_markup: {
+                  inline_keyboard: [
+                    [{ text: "Ha", callback_data: "haV" }],
+                    [{ text: "Yoq", callback_data: "yoq" }],
+                  ],
+                },
+              }
+            );
+          }
 
-          msg.telegram.sendMessage(
-            id,
-            `Accaunt rasmi  yuklandi,rasmni sizga yuboraylikmi`,
-            {
-              reply_markup: {
-                inline_keyboard: [
-                  [{ text: "Ha", callback_data: "haI" }],
-                  [{ text: "Yoq", callback_data: "yoq" }],
-                ],
-              },
-            }
-          );
+          if (res.data.body.type == "GraphImage") {
+            await data.data.pipe(
+              fs.createWriteStream(`${__dirname}/${id}.jpg`)
+            );
+
+            msg.telegram.sendMessage(
+              id,
+              `Post yuklandi,U Faylni sizga yuboraylikmi`,
+              {
+                reply_markup: {
+                  inline_keyboard: [
+                    [{ text: "Ha", callback_data: "haI" }],
+                    [{ text: "Yoq", callback_data: "yoq" }],
+                  ],
+                },
+              }
+            );
+          }
         } else {
-          if (son == 3) {
-            const base_url = "https://www.instagram.com/";
-            const url = base_url + "explore/tags/" + text + "?__a=1";
+          if (son == 2) {
+            console.log(text);
+            const rasm = await Instagram.getStories(text);
 
-            axios
-              .get(url)
-              .then((response) => {
-                console.log(response);
-                response.data.graphql.hashtag.edge_hashtag_to_media.edges.map(
-                  (post) => {
-                    if (!fs.existsSync(`./files/${text}`)) {
-                      fs.mkdirSync(`./files/${text}`, { recursive: true });
-                    }
-                    const file = fs.createWriteStream(
-                      `./files/${text}/${post.node.shortcode}.jpg`
-                    );
-                    const request = https.get(
-                      post.node.display_url,
-                      function (response) {
-                        response.pipe(file);
-                      }
-                    );
-                  }
-                );
-              })
-              .catch((err) => {
-                console.log(err);
+            if (rasm.hasError) {
+              msg.telegram.sendMessage(id, `Dasturda xatolik bor`, {
+                reply_markup: {
+                  remove_keyboard: true,
+                },
               });
+              return;
+            }
+            console.log(rasm.data);
+            data = await axios.get(rasm.data.body.profile.profile_pic_url, {
+              responseType: "stream",
+            });
 
-            son = 0;
+            await data.data.pipe(
+              fs.createWriteStream(`${__dirname}/${id}.jpg`)
+            );
+
+            msg.telegram.sendMessage(
+              id,
+              `Accaunt rasmi  yuklandi,rasmni sizga yuboraylikmi`,
+              {
+                reply_markup: {
+                  inline_keyboard: [
+                    [{ text: "Ha", callback_data: "haI" }],
+                    [{ text: "Yoq", callback_data: "yoq" }],
+                  ],
+                },
+              }
+            );
+          } else {
+            if (son == 3) {
+              const base_url = "https://www.instagram.com/";
+              const url = base_url + "explore/tags/" + text + "?__a=1";
+
+              axios
+                .get(url)
+                .then((response) => {
+                  console.log(response);
+                  response.data.graphql.hashtag.edge_hashtag_to_media.edges.map(
+                    (post) => {
+                      if (!fs.existsSync(`./files/${text}`)) {
+                        fs.mkdirSync(`./files/${text}`, { recursive: true });
+                      }
+                      const file = fs.createWriteStream(
+                        `./files/${text}/${post.node.shortcode}.jpg`
+                      );
+                      const request = https.get(
+                        post.node.display_url,
+                        function (response) {
+                          response.pipe(file);
+                        }
+                      );
+                    }
+                  );
+                })
+                .catch((err) => {
+                  console.log(err);
+                });
+
+              son = 0;
+            }
           }
         }
       }
     }
+  } catch (err) {
+    msg.telegram.sendMessage(id, `Dasturda xatolik bor`, {
+      reply_markup: {
+        remove_keyboard: true,
+      },
+    });
   }
 });
 bot.on("callback_query", async (msg) => {
-  const text = msg.update.callback_query.data;
-  const id = msg.update.callback_query.from.id;
-  if (text == "haV") {
-    const data1 = fs.readFileSync(`${__dirname}/${id}.mp4`);
-    await msg.telegram.sendVideo(
-      id,
-      {
-        source: data1,
-        filename: `${id}.mp4`,
-        remove_keyboard: true,
-      },
-      {
-        caption: "@Insta_down_aa_bot code by [Hack](https://t.me/coder_aa)",
-        parse_mode: "HTML",
-        reply_markup: {
-          keyboard: [
-            [{ text: "Post yuklash" }],
-            [{ text: "Profil Rasmini yuklash" }],
-          ],
-          resize_keyboard: true,
-        },
-      }
-    );
-
-    son = 0;
-  } else {
-    if (text == "haI") {
-      const data2 = fs.readFileSync(`${__dirname}/${id}.jpg`);
-      await msg.telegram.sendPhoto(
+  try {
+    const text = msg.update.callback_query.data;
+    const id = msg.update.callback_query.from.id;
+    if (text == "haV") {
+      const data1 = fs.readFileSync(`${__dirname}/${id}.mp4`);
+      await msg.telegram.sendVideo(
         id,
         {
-          // caption: "@Insta_down_aa_bot code by @Coder_aa",
-
-          source: data2,
-          filename: `${__dirname}/files/${id}.jpg`,
+          source: data1,
+          filename: `${id}.mp4`,
+          remove_keyboard: true,
         },
         {
+          caption: "@Insta_down_aa_bot code by [Hack](https://t.me/coder_aa)",
           parse_mode: "HTML",
-          caption:
-            "@Insta_down_aa_bot code by <a href='https://t.me/coder_aa'>Hack</a>",
           reply_markup: {
             keyboard: [
               [{ text: "Post yuklash" }],
@@ -221,8 +210,41 @@ bot.on("callback_query", async (msg) => {
           },
         }
       );
+
       son = 0;
-    } else msg.telegram.sendMessage(id, "yuklab olish yaratilmadi");
+    } else {
+      if (text == "haI") {
+        const data2 = fs.readFileSync(`${__dirname}/${id}.jpg`);
+        await msg.telegram.sendPhoto(
+          id,
+          {
+            // caption: "@Insta_down_aa_bot code by @Coder_aa",
+
+            source: data2,
+            filename: `${__dirname}/files/${id}.jpg`,
+          },
+          {
+            parse_mode: "HTML",
+            caption:
+              "@Insta_down_aa_bot code by <a href='https://t.me/coder_aa'>Hack</a>",
+            reply_markup: {
+              keyboard: [
+                [{ text: "Post yuklash" }],
+                [{ text: "Profil Rasmini yuklash" }],
+              ],
+              resize_keyboard: true,
+            },
+          }
+        );
+        son = 0;
+      } else msg.telegram.sendMessage(id, "yuklab olish yaratilmadi");
+    }
+  } catch (err) {
+    msg.telegram.sendMessage(id, `Dasturda xatolik bor`, {
+      reply_markup: {
+        remove_keyboard: true,
+      },
+    });
   }
 });
 
